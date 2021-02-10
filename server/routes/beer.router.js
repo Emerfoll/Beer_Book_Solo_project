2eebc48b1591bcd84d3728331d3531234cae9f3f
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-02-10 15:35:52 -0600

    Add beer page now adds the beer entered into the database.

--- server/routes/beer.router.js
+++ server/routes/beer.router.js
@@ -2,9 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
 router.get('/', (req, res) => {
   // GET route code here
   const queryText = `SELECT * FROM beers;`;
@@ -18,11 +16,30 @@ router.get('/', (req, res) => {
 
 });
 
-/**
- * POST route template
- */
+
+
 router.post('/', (req, res) => {
   // POST route code here
+  const beerToAdd = req.body
+  console.log(beerToAdd);
+  
+  const queryText = `INSERT INTO "my_beers" ("beer_name", "style", "abv", "brewery")
+  VALUES ($1, $2, $3, $4)
+  RETURNING "id";`;
+
+  pool.query(queryText, [beerToAdd.beer_name, 
+                        beerToAdd.style,
+                        beerToAdd.abv,
+                        beerToAdd.brewery
+                      ])
+  .then(result => {
+    console.log('New beer entry ID:', result.rows[0]);
+    
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(500)
+  })
+
 });
 
 module.exports = router;
